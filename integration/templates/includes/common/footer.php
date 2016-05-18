40b79335ba870d8361c46daffb87f02ed11aa2b2
Chizuru Shiomi <chizuru.shiomi@cgi.com>
2016-05-18 15:49:08 -0400

    UI - template, design

--- integration/templates/includes/common/footer.php
+++
@@ -1,3 +0,0 @@
-<?php 
-include_once 'includes/common/html-end.php'; 
-?>
--- integration/templates/includes/common/tag-styles.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway:300,400,700,600">
-<link rel="stylesheet" href="assets/css/main.min.css">
