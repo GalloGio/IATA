3bcdc32786e27e4732c7fdcd4f9f3365ff5a1b8a
BI\aluukka <aku.luukka@b-i.com>
2016-11-21 15:58:32 +0100

    products and services

--- src/aura/AMP_Products/AMP_ProductsController.js
+++ src/aura/AMP_Products/AMP_ProductsController.js
@@ -1,13 +1,6 @@
 ({
 	doInit: function(component, event, helper) {
-		var action;
-        action = component.get("c.canViewSalesfigures");
-        //Set up the callback
-        var self = this;
-        action.setCallback(this, function(actionResult) {
-            // console.log(JSON.stringify(actionResult.getReturnValue()));
-            component.set("v.canRead", actionResult.getReturnValue());
-        });
-        $A.enqueueAction(action);
+		helper.getCanView(component);
+		helper.getServices(component);
     },
 })
--- src/aura/AMP_Products/AMP_ProductsHelper.js
+++ src/aura/AMP_Products/AMP_ProductsHelper.js
@@ -1,4 +1,31 @@
 ({
-	helperMethod : function() {
+	getCanView : function(component) {
+		var action;
+        action = component.get("c.canViewSalesfigures");
+        //Set up the callback
+        var self = this;
+        action.setCallback(this, function(actionResult) {
+            // console.log(JSON.stringify(actionResult.getReturnValue()));
+            component.set("v.canRead", actionResult.getReturnValue());
+        });
+        $A.enqueueAction(action);
+	},
+	getServices : function(component) {
+		var action = component.get("c.getAccountServices");
+		var accountId = component.get("v.accountId");
+
+		action.setParams({
+				"accountId": accountId
+			});
+			action.setCallback(this, function(a) {
+				 var services = a.getReturnValue();
+				 var state = a.getState();
+
+				 if (component.isValid() && state === "SUCCESS") {
+					component.set("v.AccountServices", services);
+				 }
+				 else if (state === "ERROR") {}
+			});
+			$A.enqueueAction(action);
 	}
 })
