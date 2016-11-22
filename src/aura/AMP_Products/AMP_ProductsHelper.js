a0913de576d0d68587df23c9c2d279facbbda6d3
BI\aluukka <aku.luukka@b-i.com>
2016-11-22 12:40:47 +0100

    prod&serv

--- src/aura/AMP_Products/AMP_ProductsHelper.js
+++ src/aura/AMP_Products/AMP_ProductsHelper.js
@@ -22,7 +22,31 @@
 				 var state = a.getState();
 
 				 if (component.isValid() && state === "SUCCESS") {
-					component.set("v.AccountServices", services);
+					var serviceWrappers = new Array();
+					for(var i = 0; i < services.length; i++) {
+						var rowspan = 1;
+						if(i+1 < services.length) {
+							for(var j=i+1; j < services.length; j++) {
+								if(services[i].Service__r.Service_Publication__c == services[j].Service__r.Service_Publication__c) {
+									rowspan++;
+								}
+							}
+						}
+
+						if(i>0 && services[i].Service__r.Service_Publication__c == services[i-1].Service__r.Service_Publication__c) {
+							rowspan = 0;
+						}
+						var sWrapper = {
+							rowspan : rowspan,
+							Service_Publication : services[i].Service__r.Service_Publication__c,
+							Name : services[i].Service__r.Name,
+							Active : services[i].Active__c
+						};
+
+						serviceWrappers.push(sWrapper);
+					}
+					console.log(JSON.stringify(serviceWrappers));
+					component.set("v.AccountServicesWrapper", serviceWrappers);
 				 }
 				 else if (state === "ERROR") {}
 			});
