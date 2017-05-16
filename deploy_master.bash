e3f2de2e12e344257e4056d982275eae6aaeedf8
BI\aluukka <aku.luukka@b-i.com>
2017-05-16 11:05:48 +0200

    no message

--- deploy_master.bash
+++ deploy_master.bash
@@ -1,10 +1,8 @@
--#!/bin/bash -ex
--
--# Enter Build folder
--cd build
--
--# Perform the actual deployment on related repository environment
--echo "Deploying..."
--ant deployCode -Dsfdc.username=$SF_USERNAME -Dsfdc.password=$SF_PASSWORD -Dsfdc.serverurl=$SF_SERVER_URL
--
--
+#!/bin/bash -ex
+
+# Enter Build folder
+cd build
+
+# Perform the actual deployment on related repository environment
+echo "Deploying..."
+ant deployCode -Dsfdc.username=$SF_USERNAME -Dsfdc.password=$SF_PASSWORD -Dsfdc.serverurl=$SF_SERVER_URL
