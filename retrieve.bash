ff200e8882e995bf28dc530c1fba6627cfa0277a
Enrico Marnetto <marnettoe@iata.org>
2017-05-22 11:37:20 +0000

    retrieve.bash edited online with Bitbucket

--- retrieve.bash
+++ retrieve.bash
@@ -2,7 +2,7 @@
 
 cd build
 
-ant getStaticResourceAndLayouts -Dsfdc.username=$PRODun -Dsfdc.password=$PRODpw -Dsfdc.serverurl=$PRODurl -Dbasedir=$BITBUCKET_CLONE_DIR
+ant getStaticResourceAndLayouts -Dsfdc.username=$SF_USERNAME -Dsfdc.password=$SF_PASSWORD -Dsfdc.serverurl=$SF_SERVER_URL -Dbasedir=$BITBUCKET_CLONE_DIR
 
 cd ..
 
