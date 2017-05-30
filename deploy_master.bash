2bf299eee959be049c8f486c7f6fb54fe065430c
macaricol <macaricol@iata.org>
2017-05-30 11:12:00 +0000

    deploy_master.bash edited online with Bitbucket

--- deploy_master.bash
+++ deploy_master.bash
@@ -5,6 +5,6 @@ cd build
 
 # Perform the actual deployment on related repository environment
 echo "Deploying..."
-ant deployCode -Dsfdc.username=macaricol@iata.org.ngrls1trials -Dsfdc.password=Dots0Lines -Dsfdc.serverurl=$SF_SERVER_URL 
+ant deployCode -Dsfdc.username=macaricol@iata.org.ngr1trials -Dsfdc.password=Dots0Lines -Dsfdc.serverurl=$SF_SERVER_URL 
 
 
