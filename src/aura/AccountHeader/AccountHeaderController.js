3e2f65f4a370575943d87d1086dd84af326f680b
Rúben Nunes <ruben.nunes@b-i.com>
2017-06-13 13:56:55 +0100

    RN-INC322055

--- src/aura/AccountHeader/AccountHeaderController.js
+++ src/aura/AccountHeader/AccountHeaderController.js
@@ -16,31 +16,22 @@
     },
     
     switchToEditMode: function(component, event, helper) {
-        var editID = event.target.id
         var account = component.get("v.account");
         component.set("v.SanctionNotice2", account.SanctionNotice2__c);
-        if(editID=='edit2'){
             component.set("v.isEditMode2", true);
-        }
     },
     
     cancelEditMode : function(component, event, helper) {
-    var  cancelID = event.target.id;
-        if(cancelID=='cancel2'){
             component.set("v.account.SanctionNotice2__c",component.get("v.SanctionNotice2"));            
             component.set("v.isEditMode2", false);
-        }
-    },
+            },
     
     save : function(component, event, helper) {
-    var  saveID = event.target.id;
-        if(saveID=='save2'){
             var save = component.get("c.saveRecord");
             save.setParams({
                  "recordDetail": component.get("v.account")
             });
             $A.enqueueAction(save);
-            component.set("v.isEditMode2", false);
-        }
+            component.set("v.isEditMode2", false);       
     },
 })
