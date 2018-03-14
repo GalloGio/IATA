cd794748ec1836ebbc17df14de5e25dda7652892
DESKTOP-AVR4JFG\Tiago Dores <dorest@iata.com>
2018-03-14 15:55:35 +0000

    Revert "ID-7 Redirection Management / Manage IP Detection with Lightning Component (for sanction countries) - draft Version"
    This reverts commit 13a1982de2c181407fad0d83baffe1aed46bf7a1.

--- src/aura/OneIdLoginForm/OneIdLoginFormHelper.js
+++ src/aura/OneIdLoginForm/OneIdLoginFormHelper.js
@@ -191,32 +191,5 @@
     onchangeClientIpAddressAjax: function(cmp, event, helper) {
         alert("Changed field!");
         console.log("Changed field!");
-    },
-
-    getFindLocation : function (ip, component) {
-console.log('start getFindLocation');      
-
-        var action = component.get("c.getFindLocation");
-        action.setParams({"ipAddress" : ip,
-                        "saveLog" : true});        
-        
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {             
-                component.set("v.sanctionCountry",response.getReturnValue());                
-            }            
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + 
-                                 errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
-            }
-        });        
-        $A.enqueueAction(action);
-    }
+    } 
 })
