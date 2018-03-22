acc00e34442bf0503c8d23293537d1c629901ff9
DESKTOP-AVR4JFG\Tiago Dores <dorest@iata.com>
2018-03-22 11:52:09 +0000

    1ID - ISSP redirection to 1identity        - SubLogo        - client IP

--- src/aura/OneIdHeader/OneIdHeaderController.js
+++ src/aura/OneIdHeader/OneIdHeaderController.js
@@ -1,8 +1,27 @@
 ({
-	/*
-	doInit : function(component, event, helper) {
-		var vfHost = component.get("v.vfHost");
-		helper.getUrl(component, event);
+    doInit : function(component, event, helper) {
+        console.info('controller...');
+        
+        $A.get("e.c:oneIdURLParams").setParams({"state":"fetch"}).fire();
+        
+        setTimeout(function(){
+            component.set("v.loaded", true);
+        }, 2000);
+    },
+    renderPage : function (component, event, helper){
+        var state = event.getParam("state");
 
-	}*/
+        console.info("renderPage - state "+state);
+        if(state == "answer"){
+            var servName = event.getParam("paramsMap").serviceName;
+            console.info("renderPage - paramsMap ");
+            console.info(event.getParam("paramsMap"));
+            if(/\S/.test(servName)){
+                component.set("v.serviceName", servName);
+                component.set("v.customCommunity", true);
+            }
+        }
+        
+        component.set("v.loaded", true);
+    }
 })
---
+++ src/aura/OneIdParamService/OneIdParamServiceController.js
@@ -0,0 +1,25 @@
+({
+	getParams: function(component, event, helper){ 
+        console.info("Event received params ");
+        
+        if(event.getParam("state") == "fetch"){
+            var c = component.get("v.checked");
+        
+            if(!c){
+                var urlParameters = decodeURIComponent(window.location.search.substring(1)).toString(); // Right part after base URL
+                var paramsMap = JSON.parse('{"' + urlParameters.replace(new RegExp('"', 'g'), '\\"').replace(new RegExp('&', 'g'), '","').replace(new RegExp('=', 'g'),'":"') + '"}'); 
+                
+                component.set("v.url", urlParameters);
+                component.set("v.paramsMap", paramsMap);
+                component.set("v.checked", true);
+               
+            }
+            
+            $A.get("e.c:oneIdURLParams").setParams({
+                "state" : "answer",
+                "url" : component.get("v.url"),
+                "paramsMap" : component.get("v.paramsMap")
+            }).fire();
+        }
+    }
+})
