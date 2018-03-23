4e0baf805e020e116241cc1435818a7868ebec98
DESKTOP-AVR4JFG\Tiago Dores <dorest@iata.com>
2018-03-23 15:11:14 +0000

    ID1 - redirection management | Sanction Countries

--- src/aura/Base/BaseHelper.js
+++ src/aura/Base/BaseHelper.js
@@ -1,10 +1,9 @@
 ({
-	getParams: function(component, event, helper){ 
+	getParam: function(component, event, helper){ 
         console.info("Event received params ");
         
         if(event.getParam("state") == "fetch"){
-            var c = component.get("v.checked");
-        
+            var c = component.get("v.checked");        
             if(!c){
                 var urlParameters = decodeURIComponent(window.location.search.substring(1)).toString(); // Right part after base URL
                 var paramsMap = JSON.parse('{"' + urlParameters.replace(new RegExp('"', 'g'), '\\"').replace(new RegExp('&', 'g'), '","').replace(new RegExp('=', 'g'),'":"') + '"}'); 
