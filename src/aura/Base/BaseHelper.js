5e800cff73feb9e85b94e77b8869c5a697c0f4d1
DESKTOP-AVR4JFG\Tiago Dores <dorest@iata.com>
2018-03-24 14:47:43 +0000

    1ID - redirection management

--- src/aura/Base/BaseHelper.js
+++ src/aura/Base/BaseHelper.js
@@ -1,12 +1,14 @@
 ({
-	getParam: function(component, event, helper){ 
+    getParam: function(component, event, helper){ 
         console.info("Event received params ");
         
         if(event.getParam("state") == "fetch"){
-            var c = component.get("v.checked");        
+            var c = component.get("v.checked");
+                    
             if(!c){
-                var urlParameters = decodeURIComponent(window.location.search.substring(1)).toString(); // Right part after base URL
-                var paramsMap = JSON.parse('{"' + urlParameters.replace(new RegExp('"', 'g'), '\\"').replace(new RegExp('&', 'g'), '","').replace(new RegExp('=', 'g'),'":"') + '"}'); 
+                var urlParamEncoded = window.location.search.substring(1).toString();
+                var urlParameters = decodeURIComponent(urlParamEncoded); // Right part after base URL
+                var paramsMap = JSON.parse((urlParameters == '') ? '{}' : decodeURIComponent('{"' + urlParamEncoded.replace(new RegExp('&', 'g'), '","').replace(new RegExp('=', 'g'),'":"') + '"}') );
                 
                 component.set("v.url", urlParameters);
                 component.set("v.paramsMap", paramsMap);
