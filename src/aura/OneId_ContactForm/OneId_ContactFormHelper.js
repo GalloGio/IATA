bdf04ce6e38aafb12b0242d54332293ca08eeafc
BI\emarnetto <marnettoe@iata.org>
2018-06-06 09:35:11 +0200

    EM - fix internal sfdc bug

--- src/aura/OneId_ContactForm/OneId_ContactFormHelper.js
+++ src/aura/OneId_ContactForm/OneId_ContactFormHelper.js
@@ -34,7 +34,7 @@
 
         var salutation = component.find("salutation");
         if($A.util.isEmpty(salutation.get("v.value"))) {
-            salutation.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_Salutation")}]);
+            salutation.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_Salutation")}]);
             isAllFilled = false;
         } else {
             salutation.set("v.errors", null);
@@ -42,7 +42,7 @@
 
         var firstName = component.find("firstName");
         if($A.util.isEmpty(firstName.get("v.value"))) {
-            firstName.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_FirstName")}]);
+            firstName.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_FirstName")}]);
             isAllFilled = false;
         } else {
             firstName.set("v.errors", null);
@@ -50,7 +50,7 @@
 
         var lastName = component.find("lastName");
         if($A.util.isEmpty(lastName.get("v.value"))) {
-            lastName.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_LastName")}]);
+            lastName.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_LastName")}]);
             isAllFilled = false;
         } else {
             lastName.set("v.errors", null);
@@ -58,7 +58,7 @@
 
         var title = component.find("title");
         if($A.util.isEmpty(title.get("v.value"))) {
-            title.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_JobTitle")}]);
+            title.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_JobTitle")}]);
             isAllFilled = false;
         } else {
             title.set("v.errors", null);
@@ -66,7 +66,7 @@
 
         var job = component.find("membershipFunction");
         if($A.util.isEmpty(job.get("v.value"))) {
-            job.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_JobFunction")}]);
+            job.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_JobFunction")}]);
             isAllFilled = false;
             $A.util.addClass(component.find("jobHelp"), "jobHelp");
         } else {
@@ -80,7 +80,7 @@
             var domPhone = phone.getElement();
             var country = $(domPhone).intlTelInput("getSelectedCountryData").iso2;
 
-            phone.set("v.errors", [{message:$A.get("$Label.c.ISSP_Registration_Error_BusinessPhone")}]);
+            phone.set("v.errors", [{message: $A.get("$Label.c.ISSP_Registration_Error_BusinessPhone")}]);
             if(!$A.util.hasClass(container, 'slds-has-error')) $A.util.addClass(container, 'slds-has-error');
             isAllFilled = false;
 
