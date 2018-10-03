8695fa297637af9710ac5d380bb965cb682a2e11
Pedro Fartaria <pedro.fartaria@b-i.com>
2018-10-03 15:12:15 +0100

    Package creation elements

--- deploy_master.bash
+++ deploy_master.bash
@@ -1,10 +1,11 @@
 #!/bin/bash -ex
+serverurl=https://test.salesforce.com
 
 # Enter Build folder
 cd build
 
 # Perform the actual deployment on related repository environment
 echo "Deploying..."
-ant deployCode -Dsfdc.username=$SF_USERNAME -Dsfdc.password=$SF_PASSWORD -Dsfdc.serverurl=$SF_SERVER_URL
+ant validateCodeLocal -Dsfdc.username=$username -Dsfdc.password=$password -Dsfdc.serverurl=$serverurl
 
 
