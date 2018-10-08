dc37ff47042b0686bdc5daac047f9cefea36ceeb
BI\dtullo <tullod@iata.org>
2018-10-08 17:04:58 +0200

    remove unecessary files

--- deploy_master.bash
+++ deploy_master.bash
@@ -1,11 +1,10 @@
 #!/bin/bash -ex
-serverurl=https://test.salesforce.com
 
 # Enter Build folder
 cd build
 
 # Perform the actual deployment on related repository environment
 echo "Deploying..."
-ant validateCodeLocal -Dsfdc.username=$username -Dsfdc.password=$password -Dsfdc.serverurl=$serverurl
+ant deployCode -Dsfdc.username=$SF_USERNAME -Dsfdc.password=$SF_PASSWORD -Dsfdc.serverurl=$SF_SERVER_URL
 
 
