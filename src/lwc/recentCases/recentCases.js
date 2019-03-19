cb9a47cce58e7af1e3f478f37a0ff6c9e129fdb3
jonathantaroco <tarocojonathan@gmail.com>
2019-03-19 18:56:26 +0000

    MTSL-66 Add Custom Label and Method to Get Field Labels

--- src/lwc/recentCases/recentCases.js
+++ src/lwc/recentCases/recentCases.js
@@ -1,39 +1,47 @@
 import { LightningElement, track, wire } from 'lwc';
 import getRecentCases from '@salesforce/apex/PortalCasesCtrl.getRecentCases';
+import getSelectedColumns from '@salesforce/apex/CSP_Utils.getSelectedColumns';
 import CSP_NoCases1 from '@salesforce/label/c.CSP_NoCases1';
 import CSP_NoCases2 from '@salesforce/label/c.CSP_NoCases2';
-import ISSP_CountryConcerned from '@salesforce/label/c.ISSP_CountryConcerned';
-import CSP_CaseNumber from '@salesforce/label/c.CSP_CaseNumber';
-import CSP_TypeOfCase from '@salesforce/label/c.CSP_TypeOfCase';
-import CurrencyCenter_Subject from '@salesforce/label/c.CurrencyCenter_Subject';
-import Created_Date from '@salesforce/label/c.Created_Date';
-import CSP_Status from '@salesforce/label/c.CSP_Status';
 import CSP_RecentCases from '@salesforce/label/c.CSP_RecentCases';
+import CSP_SeeAll from '@salesforce/label/c.CSP_SeeAll';
+import CSP_Question1 from '@salesforce/label/c.CSP_Question1';
+import CSP_Question2 from '@salesforce/label/c.CSP_Question2';
 
 export default class RecentCases extends LightningElement {
     label = {
         CSP_NoCases1,
         CSP_NoCases2,
-        ISSP_CountryConcerned,
-        CSP_CaseNumber,
-        CSP_TypeOfCase,
-        CurrencyCenter_Subject,
-        Created_Date,
-        CSP_Status,
-        CSP_RecentCases
+        CSP_RecentCases,
+        CSP_SeeAll,
+        CSP_Question1,
+        CSP_Question2
     };
-    
     @track error;
     @track data;
-    @track columns = [
-        { label: this.label.CSP_CaseNumber, fieldName: 'CaseNumber', type: 'text' },
-        { label: this.label.CSP_TypeOfCase, fieldName: 'Type_of_case_Portal__c', type: 'text' },
-        { label: this.label.CurrencyCenter_Subject, fieldName: 'Subject', type: 'text' },
-        { label: this.label.ISSP_CountryConcerned, fieldName: 'Country_concerned_by_the_query__c', type: 'text' },
-        { label: this.label.Created_Date, fieldName: 'CreatedDate', type: 'date', typeAttributes: { year: "numeric", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" } },
-        { label: this.label.CSP_Status, fieldName: 'Portal_Case_Status__c', type: 'text' }
-    ];
+    @track columns;
     @track loading = true;
+
+    fieldLabels = [
+        'CaseNumber', 'Type_of_case_Portal__c', 'Subject', 'Country_concerned_by_the_query__c', 'CreatedDate', 'Portal_Case_Status__c'
+    ];
+
+    connectedCallback() {
+        getSelectedColumns({ sObjectType : 'Case', sObjectFields : this.fieldLabels })
+        .then(results => {           
+                this.columns = [
+                    {label: results.CaseNumber, fieldName: 'CaseNumber', type: 'text'},
+                    {label: results.Type_of_case_Portal__c, fieldName: 'Type_of_case_Portal__c', type: 'text'},
+                    {label: results.Subject, fieldName: 'Subject', type: 'text'},
+                    {label: results.Country_concerned_by_the_query__c, fieldName: 'Country_concerned_by_the_query__c', type: 'text'},
+                    {label: results.CreatedDate, fieldName: 'CreatedDate', type: 'date', typeAttributes: {year: "numeric", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit"}},
+                    {label: results.Portal_Case_Status__c, fieldName: 'Portal_Case_Status__c', type: 'text'}
+                ];
+        })
+        .catch(error => {
+            this.error = error;
+        }); 
+    }
 
     @wire(getRecentCases)
     wiredRecentCases(results) {
