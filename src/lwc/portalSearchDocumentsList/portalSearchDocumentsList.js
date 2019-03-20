7d6a73505ad58cb2c6b0c01adde5a05738e771bc
jonathantaroco <tarocojonathan@gmail.com>
2019-03-20 10:03:59 +0000

    MTSL-102 Add Custom Labels and Code Review

--- src/lwc/portalSearchDocumentsList/portalSearchDocumentsList.js
+++ src/lwc/portalSearchDocumentsList/portalSearchDocumentsList.js
@@ -1,17 +1,39 @@
 import { LightningElement, api, track } from 'lwc';
 import getSearchDocuments from '@salesforce/apex/PortalDocumentsController.getSearchDocuments';
+import getSelectedColumns from '@salesforce/apex/CSP_Utils.getSelectedColumns';
+import CSP_NoSearchResults from '@salesforce/label/c.CSP_NoSearchResults';
+import CSP_SeeAll from '@salesforce/label/c.CSP_SeeAll';
+import CSP_Documents from '@salesforce/label/c.CSP_Documents';
 
 export default class PortalSearchDocumentsList extends LightningElement {
+    label = {
+        CSP_NoSearchResults,
+        CSP_SeeAll,
+        CSP_Documents
+    };    
     @track dataRecords = false;
     @track filteringObject;
     @track loading = true;
     @track error;
     @track data;
-    @track columns = [
-        {label: 'Title', fieldName: 'Title', type: 'text'},
-        {label: '', fieldName: '', cellAttributes:
-                { iconName: 'utility:forward', iconPosition: 'right' }} 
+    @track columns;
+
+    fieldLabels = [
+        'Title'
     ];
+
+    connectedCallback() {
+        getSelectedColumns({ sObjectType : 'ContentVersion', sObjectFields : this.fieldLabels })
+        .then(results => {           
+                this.columns = [
+                    {label: results.Title, fieldName: 'Title', type: 'text'},
+                    {label: '', fieldName: '', cellAttributes: { iconName: 'utility:forward', iconPosition: 'right' }}
+                ];
+        })
+        .catch(error => {
+            this.error = error;
+        }); 
+    }
 
     @api
     get filteringObjectParent() {
