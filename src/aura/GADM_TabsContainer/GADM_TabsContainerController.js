9635e51d37ceaa41c6ac718c0a6e05f200b7b033
jcermak <jiri.cermak@b-i.com>
2019-05-15 16:39:11 +0200

    Portal Tabs visibility - IATAIGP-47

--- src/aura/GADM_TabsContainer/GADM_TabsContainerController.js
+++ src/aura/GADM_TabsContainer/GADM_TabsContainerController.js
@@ -1,17 +1,15 @@
 ({
     init : function(component, event, helper) {
         helper.handleTrackUsage(component, event);
-    },
-
-	handleApplicationEvent  : function(cmp, event) {
-        var tabIndexReceived = event.getParam("activeTabId");
-        cmp.set("v.tabId", tabIndexReceived);
-        cmp.set("v.doDisplayTab", true);
-        cmp.find("tabs").set("v.selectedTabId", tabIndexReceived);
+        helper.handleGetUserInformation(component, event);
     },
 
     trackUsage : function(component, event, helper) {
         helper.handleTrackUsage(component, event);
+    },
+
+    getUserInformation : function(component, event, helper) {
+        helper.handleGetUserInformation(component, event);
     },
 
 
--- src/aura/GADM_TabsContainer/GADM_TabsContainerHelper.js
+++ src/aura/GADM_TabsContainer/GADM_TabsContainerHelper.js
@@ -44,11 +44,65 @@
         tabMap.set('2', 'DATA SUBMISSION');
         tabMap.set('3', 'DATA SUBMISSION RESULTS');
         tabMap.set('4', 'USER MANAGEMENT');
-        tabMap.set('5', 'PORTAL USAGE');
-        tabMap.set('6', 'DASHBOARDS');
+        tabMap.set('5', 'DASHBOARDS');
 
         var result = tabMap.get(tabId);
         return tabMap.get(tabId);
+    },
+
+    handleGetUserInformation : function(component, event) {
+        debugger;
+        let id = $A.get('$SObjectType.CurrentUser.Id');
+        let action = component.get('c.getUserGrantedRoles');
+        action.setParams({
+            'userId' : $A.get('$SObjectType.CurrentUser.Id')
+        });
+        action.setCallback(this, function(response){
+            const state = response.getState();
+            if(state === 'SUCCESS') {
+                const grantedRoles = response.getReturnValue();
+                if(! $A.util.isEmpty(grantedRoles)) {
+
+                    let isDataSubmitter = false;
+                    let isDataConsumer = false;
+
+                    for(let i in grantedRoles) {
+
+                        if(grantedRoles[i].Name === 'GADM Data Submitter') {
+                            isDataSubmitter = true;
+                            continue;
+                        }
+
+                        if(grantedRoles[i].Name === 'GADM Data Consumer') {
+                            isDataConsumer = true;
+                            continue;
+                        }
+
+                    }
+
+                    if(isDataSubmitter) {
+                        component.set('v.isDataSubmissionVisible', true);
+                        component.set('v.isDataSubmissionResultVisible', true);
+                    }
+
+                    if(isDataConsumer) {
+                        component.set('v.isDashboardsVisible', true);
+                    }
+
+                    component.set('v.doDisplayTab', true);
+
+                }else{
+                    console.log('handleGetUserInformation error - empty user granted roles retrieved');
+
+                }
+
+            }else{
+                console.log('handleGetUserInformation error - cannot retrieve user granted roles');
+
+            }
+
+        });
+        $A.enqueueAction(action);
     },
 
 })
