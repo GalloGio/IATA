43ae5a7cc5331aaee950fb9c83367633c2643a66
jcermak <jiri.cermak@b-i.com>
2019-06-06 13:48:28 +0200

    GADMWrapper - getGadmUserRoleStatus changes + GADM Account Service Detail layout changes

--- src/aura/GADMwrapper/GADMwrapperHelper.js
+++ src/aura/GADMwrapper/GADMwrapperHelper.js
@@ -9,33 +9,22 @@
         action.setCallback(this, function(response){
             const state = response.getState();
             if(state === 'SUCCESS') {
-                const result = response.getReturnValue();
+                const status = response.getReturnValue();
 
-                if(! $A.util.isEmpty(result)) {
+                if(! $A.util.isEmpty(status)) {
 
-                    if(! $A.util.isEmpty(result['GADM User'])) {
+                    if(status === 'Granted') {// access status is granted, check account access status
+                        this.checkAccountStatus(component, event);
+                    }
 
-                        let accessStatus = result['GADM User'];
+                    if(status === 'Pending') {
+                        component.set('v.accessMessage', $A.get("$Label.c.GADM_Access_Pending"));
+                        component.set('v.accessDenied', true);
+                        this.toggleSpinner(component);
+                    }
 
-                        if(accessStatus === 'Granted') {// access status is granted, check account access status
-                            this.checkAccountStatus(component, event);
-                        }
-
-                        if(accessStatus === 'Pending') {
-                            component.set('v.accessMessage', $A.get("$Label.c.GADM_Access_Pending"));
-                            component.set('v.accessDenied', true);
-                            this.toggleSpinner(component);
-                        }
-
-                        if(accessStatus === 'Denied') {
-                            component.set('v.accessMessage', $A.get("$Label.c.GADM_Access_Denied"));
-                            component.set('v.accessDenied', true);
-                            this.toggleSpinner(component);
-                        }
-
-                    }else{
-                        //no access to gadm
-                        component.set('v.accessMessage', $A.get("$Label.c.GADM_No_Access"));
+                    if(status === 'Denied') {
+                        component.set('v.accessMessage', $A.get("$Label.c.GADM_Access_Denied"));
                         component.set('v.accessDenied', true);
                         this.toggleSpinner(component);
                     }
@@ -88,25 +77,27 @@
                                     component.set('v.accessGranted', true);
 
                                 }else{//effective is not valid
+
                                     component.set('v.accessMessage', $A.get("$Label.c.GADM_account_not_effective"));
                                     component.set('v.accessDenied', true);
                                 }
                             }else{//empty effective
-                                //TODO:new label for error message
+
                                 component.set('v.accessMessage', $A.get("$Label.c.GADM_No_Access"));
                                 component.set('v.accessDenied', true);
                             }
                         }else{//status is not active
+
                             component.set('v.accessMessage', $A.get("$Label.c.GADM_Account_not_active"));
                             component.set('v.accessDenied', true);
                         }
                     }else{//empty status
-                        //TODO:new label for error message
+
                         component.set('v.accessMessage', $A.get("$Label.c.GADM_No_Access"));
                         component.set('v.accessDenied', true);
                     }
                 }else{//empty response
-                    //TODO:new label for error message
+
                     component.set('v.accessMessage', $A.get("$Label.c.GADM_No_Access"));
                     component.set('v.accessDenied', true);
                 }
