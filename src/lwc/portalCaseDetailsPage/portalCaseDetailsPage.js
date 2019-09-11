8237c92e686aea57dbbf584a57f31641f2d6814a
DESKTOP-8GDFJM4\Tiago Fernandes <tiagojorge.fernandes@b-i.com>
2019-09-11 16:03:38 +0100

    MTSL-380 - Changes made based on the Demo comments - Oscar Progress Bar re-styling;

--- src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
+++ src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
@@ -1,33 +1,3 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
-import getOscarProgress from '@salesforce/apex/portal_OscarProgressBar.getOscarProgress';
-
-import { getParamsFromPage } from'c/navigationUtils';
-
-export default class PortalCaseDetailsPage extends LightningElement {
-
-    @track displayOscarProgressBar = false;
-    @track progressStatusList = [];
-
-    connectedCallback() {
-
-        this.pageParams = getParamsFromPage();
-
-        if(this.pageParams.caseId !== undefined){
-            this.getProgressBarStatus();
-        }
-
-    }
-
-    getProgressBarStatus() {
-        getOscarProgress({ caseId : this.pageParams.caseId })
-        .then(results => {
-            if (results.length > 0){
-                this.displayOscarProgressBar = true;
-                this.progressStatusList = results;
-            }
-        });
-
-    }
-
-}
+export default class PortalCaseDetailsPage extends LightningElement {}
