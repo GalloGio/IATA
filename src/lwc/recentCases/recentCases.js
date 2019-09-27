2cd0b7078e35023d06b715f6e90d7377e5b7efa1
Jorge Baptista <Jorge.baptista@b-i.com>
2019-09-27 15:25:27 +0100

    MTSL-954
    - Added a Tooltip with the Case Number and Subject on the correspondent fields.

--- src/lwc/recentCases/recentCases.js
+++ src/lwc/recentCases/recentCases.js
@@ -67,9 +67,9 @@ export default class RecentCases extends NavigationMixin(LightningElement) {
                 this.cardBodyContent = "cardBodyContent";
                 this.rowHeight = "";
                 this.columns = [
-                    { label: results.CaseNumber, fieldName: 'CaseURL', type: 'url', initialWidth: 137, typeAttributes: {label: {fieldName: 'CaseNumber'}, target:'_self'} },
+                    { label: results.CaseNumber, fieldName: 'CaseURL', type: 'url', initialWidth: 137, typeAttributes: {label: {fieldName: 'CaseNumber'}, target:'_self',tooltip: {fieldName: 'CaseNumber'}} },
                     { label: results.Type_of_case_Portal__c, fieldName: 'Type_of_case_Portal__c', type: 'text', initialWidth: 130 },
-                    { label: results.Subject, fieldName: 'CaseURL', type: 'url', typeAttributes: {label: {fieldName: 'Subject'}, target:'_self'}, cellAttributes: {class: 'slds-text-title_bold text-black'} },
+                    { label: results.Subject, fieldName: 'CaseURL', type: 'url', typeAttributes: {label: {fieldName: 'Subject'}, target:'_self',tooltip:{fieldName: 'Subject'}}, cellAttributes: {class: 'slds-text-title_bold text-black'} },
                     { label: results.Country_concerned__c, fieldName: 'Country', type: 'text' },
                     { label: results.Portal_Case_Status__c, fieldName: 'Portal_Case_Status__c', type: 'text', initialWidth: 140, cellAttributes: { class: { fieldName: 'statusClass' } } }
                 ];
