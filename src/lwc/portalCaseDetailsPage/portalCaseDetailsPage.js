0d3dd68e0a4718f7e93b9d50584afc7b48bf23e8
Miguel Alves Vieira <miguel.vieira@b-i.com>
2019-10-17 18:29:28 +0100

    MTSL-870 : JS added function to print window and function to print date of print action, adde print button and hidden logo for PDF to HTML, added CSS to correctly format PDF file

--- src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
+++ src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import CSP_PortalPath from '@salesforce/label/c.CSP_PortalPath';
 
-export default class PortalCaseDetailsPage extends LightningElement {}
+export default class PortalCaseDetailsPage extends LightningElement {
+    @track now = '';
+
+    //links for images
+    logoIcon = CSP_PortalPath + 'CSPortal/Images/Logo/group.svg';
+    printIcon = CSP_PortalPath + 'CSPortal/Images/Icons/print-blue.svg';
+
+    downloadedOn() {
+        let d = new Date();
+        let n = d.toLocaleString();
+        this.now = n;
+    }
+    toPDF() {
+        window.print();
+    }
+
+}
