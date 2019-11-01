754fc3fe13fe6d903b70e87c04f71c05c4d96a2a
Tiago Alvané <tiago.alvane@isobar.com>
2019-11-01 16:25:26 +0000

    TA-PRB41772: Could not download Notice of Change

--- src/lwc/portalCaseDetailsDetails/portalCaseDetailsDetails.js
+++ src/lwc/portalCaseDetailsDetails/portalCaseDetailsDetails.js
@@ -224,8 +224,7 @@ export default class PortalCaseDetailsDetails extends LightningElement {
 
     get getPDF2_3() {
         if (this.caseDetails) {
-            return this.caseDetails.Reason1__c === 'Bank Detail Update' || this.caseDetails.Reason1__c.startsWith('CH') ||
-                this.caseDetails.Reason1__c.startsWith('CL') || this.caseDetails.Reason1__c === 'Major Change';
+            return this.caseDetails.Reason1__c === 'Bank Detail Update' || this.caseDetails.Reason1__c.startsWith('CH') || this.caseDetails.Reason1__c.startsWith('CL') || this.caseDetails.Reason1__c === 'Major Change' || this.optionBuilder.isNoticeOfChange;
         }
         return null;
     }
