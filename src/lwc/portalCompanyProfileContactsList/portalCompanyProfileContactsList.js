4bcb520ed0166da15e0c623387cb5bed2ab67bf8
jonathantaroco <tarocojonathan@gmail.com>
2019-11-04 16:58:57 +0000

    Fix on refresh contact list

--- src/lwc/portalCompanyProfileContactsList/portalCompanyProfileContactsList.js
+++ src/lwc/portalCompanyProfileContactsList/portalCompanyProfileContactsList.js
@@ -124,6 +124,7 @@ export default class PortalCompanyProfileContactsList extends LightningElement {
 
     //resets and recalls the first branches page
     resetContactsList(){
+        this.contactsLoaded = false;
         this.contacts = [];
         this.contactsFilteringObject.searchInput = '';
         this.currentPage = 1;
