aae380b43516a9b821eb8a203ed03f14672af603
Timur Begishev <timurb@nubessom.com>
2019-11-20 21:34:55 +0300

    INC484202 additional dev

--- src/aura/closeAllTabs/CloseAllTabsRenderer.js
+++ src/aura/closeAllTabs/CloseAllTabsRenderer.js
@@ -1,5 +1,16 @@
 ({
+	afterRender: function(component, helper)
+	{
+        this.superAfterRender();
 
-// Your renderer method overrides go here
+        var utilityBarAPI = component.find("utilitybar");
+        var eventHandler = function(response){
+            helper.getAllTabInfo(component);
+        };
 
+        utilityBarAPI.onUtilityClick({ 
+            eventHandler: eventHandler 
+        })
+
+    }        
 })
