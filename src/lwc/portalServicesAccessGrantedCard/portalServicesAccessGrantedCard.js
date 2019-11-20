790ea4c18388651bf92cc2c1d24eb1b424d027a6
Luis Maçarico <luis.macarico@isobar.com>
2019-11-20 11:05:38 +0000

    whitespaces to tabs

--- src/lwc/portalServicesAccessGrantedCard/portalServicesAccessGrantedCard.js
+++ src/lwc/portalServicesAccessGrantedCard/portalServicesAccessGrantedCard.js
@@ -17,85 +17,85 @@ import CSP_Services_GoToService from '@salesforce/label/c.CSP_Services_GoToServi
 
 export default class PortalServicesAccessGrantedCard extends NavigationMixin(LightningElement) {
 
-    @api service;
+	@api service;
 
-    label = {
-        CSP_Services_ManageService,
-        CSP_Services_GoToService
-    };
+	label = {
+		CSP_Services_ManageService,
+		CSP_Services_GoToService
+	};
 
-    goToManageServiceButtonClick(event) {
-        let serviceAux = JSON.parse(JSON.stringify(this.service));
+	goToManageServiceButtonClick(event) {
+		let serviceAux = JSON.parse(JSON.stringify(this.service));
 
-        let params = {};
-        params.serviceId = serviceAux.recordService.Id;
+		let params = {};
+		params.serviceId = serviceAux.recordService.Id;
 
-        event.preventDefault();
-        event.stopPropagation();
-        this[NavigationMixin.GenerateUrl]({
-            type: "standard__namedPage",
-            attributes: {
-                pageName: "manage-service"
-            }})
-            .then(url => navigateToPage(url, params));
-    }
+		event.preventDefault();
+		event.stopPropagation();
+		this[NavigationMixin.GenerateUrl]({
+			type: "standard__namedPage",
+			attributes: {
+				pageName: "manage-service"
+			}})
+			.then(url => navigateToPage(url, params));
+	}
 
-    goToServiceButtonClick() {
-        //because proxy.......
-        let serviceAux = JSON.parse(JSON.stringify(this.service)).recordService;
+	goToServiceButtonClick() {
+		//because proxy.......
+		let serviceAux = JSON.parse(JSON.stringify(this.service)).recordService;
 
-        //attributes stored on element that is related to the event
-        let appUrlData = serviceAux.Application_URL__c
-        let appFullUrlData = serviceAux.Application_URL__c;
-        let openWindowData = serviceAux.New_Window__c;
-        let requestable = serviceAux.Requestable__c;
-        let recordId = serviceAux.Id;
-        
-        // update Last Visit Date on record
-        updateLastModifiedService({ serviceId: recordId })
+		//attributes stored on element that is related to the event
+		let appUrlData = serviceAux.Application_URL__c
+		let appFullUrlData = serviceAux.Application_URL__c;
+		let openWindowData = serviceAux.New_Window__c;
+		let requestable = serviceAux.Requestable__c;
+		let recordId = serviceAux.Id;
 
-        let myUrl;
-        let flag = false;
-        if (appUrlData !== '') {
-            myUrl = appUrlData;
-            flag = true;
-        } else if (appFullUrlData !== '') {
-            myUrl = appFullUrlData;
-            flag = true;
-        }
+		// update Last Visit Date on record
+		updateLastModifiedService({ serviceId: recordId })
 
-        if (flag) {
-            //verifies if the event target contains all data for correct redirection
-            if (openWindowData !== null && openWindowData !== undefined) {
-                //determines if the link is to be opened on a new window or on the current
-                if (openWindowData) {
-                    //open new tab with the redirection
+		let myUrl;
+		let flag = false;
+		if (appUrlData !== '') {
+			myUrl = appUrlData;
+			flag = true;
+		} else if (appFullUrlData !== '') {
+			myUrl = appFullUrlData;
+			flag = true;
+		}
 
-                    if (myUrl.startsWith('/')) {
-                        goToOldPortalService({ myurl: myUrl })
-                            .then(result => {
-                                //open new tab with the redirection
-                                window.open(result);
-                                this.toggleSpinner();
-                            })
-                            .catch(error => {
-                                //throws error
-                                this.error = error;
-                            });
+		if (flag) {
+			//verifies if the event target contains all data for correct redirection
+			if (openWindowData !== null && openWindowData !== undefined) {
+				//determines if the link is to be opened on a new window or on the current
+				if (openWindowData) {
+					//open new tab with the redirection
 
-                    } else {
-                        if(serviceAux.ServiceName__c === 'Training Platform (LMS)'){
+					if (myUrl.startsWith('/')) {
+						goToOldPortalService({ myurl: myUrl })
+							.then(result => {
+								//open new tab with the redirection
+								window.open(result);
+								this.toggleSpinner();
+							})
+							.catch(error => {
+								//throws error
+								this.error = error;
+							});
+
+					} else {
+						if(serviceAux.ServiceName__c === 'Training Platform (LMS)'){
 							getPortalServiceId({ serviceName: serviceAux.ServiceName__c })
 								.then(serviceId => {
 									console.log('service Id: ' + recordId);
 									verifyCompleteL3Data({serviceId: recordId})
 									.then(result => {
 										if(result){
-                                            console.log('to service');
+											console.log('to service');
 											window.open(myUrl);
 										}
 										else{
-                                            console.log('fire event')
+											console.log('fire event')
 											fireEvent(this.pageRef, 'fireL3Registration', serviceId);
 
 										}
@@ -111,38 +111,38 @@ export default class PortalServicesAccessGrantedCard extends NavigationMixin(Lig
 
 						}
 						else{
-                            if (!myUrl.startsWith('http')) {
-                                myUrl = window.location.protocol + '//' + myUrl;
-                            }
-                            window.open(myUrl);
-                            this.toggleSpinner();
-                        }
-                    }
+							if (!myUrl.startsWith('http')) {
+								myUrl = window.location.protocol + '//' + myUrl;
+							}
+							window.open(myUrl);
+							this.toggleSpinner();
+						}
+					}
 
 
-                } else if (myUrl !== '') {
-                    //redirects on the same page
-                    //method that redirects the user to the old portal maintaing the same loginId
-                    goToOldPortalService({ myurl: myUrl })
-                        .then(result => {
-                            //open new tab with the redirection
-                            window.location.href = result;
-                            this.toggleSpinner();
-                        })
-                        .catch(error => {
-                            //throws error
-                            this.error = error;
-                        });
+				} else if (myUrl !== '') {
+					//redirects on the same page
+					//method that redirects the user to the old portal maintaing the same loginId
+					goToOldPortalService({ myurl: myUrl })
+						.then(result => {
+							//open new tab with the redirection
+							window.location.href = result;
+							this.toggleSpinner();
+						})
+						.catch(error => {
+							//throws error
+							this.error = error;
+						});
 
-                }
-            }
-        } else {
-            console.info('No link to the service has been set.')
-        }
+				}
+			}
+		} else {
+			console.info('No link to the service has been set.')
+		}
 
 
-    }
+	}
 
-    @wire(CurrentPageReference) pageRef;
+	@wire(CurrentPageReference) pageRef;
 
 }
