ad9c098dc0add49dad2f905c086baa9f6dee8527
Mário Ramalho <>
2019-12-19 13:23:03 +0000

    WMO-797 WMO-884: For airline profiles, there is a show more/less functionality.

---
+++ src/aura/AWW_AccountPlan/AWW_AccountPlanController.js
@@ -0,0 +1,55 @@
+({
+    doInit : function(component,event,helper) {
+        helper.checkUserHaveAccessRightsAMPIssuesAndPriorities(component);
+    },
+    viewCurrent : function(component,event,helper) {
+        component.set('v.activityView', 'current');
+        component.set('v.data', component.get('v.currentData'));
+        if(component.get('v.sortBy')) {
+            helper.sortData(component,component.get('v.sortBy'),component.get('v.sortDirection'));
+        }
+    },
+    viewHistory : function(component,event,helper) {
+        component.set('v.activityView', 'history');
+        component.set('v.data', component.get('v.historyData'));
+        if(component.get('v.sortBy')) {
+            helper.sortData(component,component.get('v.sortBy'),component.get('v.sortDirection'));
+        }
+    },
+    refreshTab : function(component,event,helper) {
+        if(event.getParam('tabName') == 'account_plan') {
+            helper.fetchData(component);
+        }
+    },
+    handleRowAction : function(component,event,helper) {
+        var action = event.getParam('action').name;
+        switch (action) {
+            case 'edit_row':
+                helper.editRecord(component, event);
+                break;
+            case 'delete_row':
+                helper.deleteRecord(component, event);
+                break;
+            case 'show_milestones':
+                helper.showMilestones(component, event);
+                break;
+        }
+    },
+    new : function(component,event,helper) {
+        helper.newRecord(component,event);
+    },
+    closeMilestones : function(component,event,helper) {
+        component.set('v.milestoneView',false);
+    },
+    print : function(component,event,helper) {
+        var accountId = component.get('v.accountId');
+        window.open('/apex/AMP_KeyAccountPlanPrintable?accountId=' +  accountId);
+    },
+    handleSort : function(component,event,helper) {
+        var sortBy = event.getParam('fieldName');
+        var sortDirection = event.getParam('sortDirection');
+        component.set('v.sortBy',sortBy);
+        component.set('v.sortDirection',sortDirection);
+        helper.sortData(component,sortBy,sortDirection);
+    }  
+})
