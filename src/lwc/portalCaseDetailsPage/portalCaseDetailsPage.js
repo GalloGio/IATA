9614c4af58e120a2e6e1e2133106ea6dd26c3ccf
victorval <victor.valinho@isobar.com>
2020-01-16 18:30:12 +0000

    INC509745 - Community 360 not creating any data
    Test Class: Community360CtrlTest

--- src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
+++ src/lwc/portalCaseDetailsPage/portalCaseDetailsPage.js
@@ -1,11 +1,16 @@
 import { LightningElement, track } from 'lwc';
 import CSP_PortalPath from '@salesforce/label/c.CSP_PortalPath';
+
+import { getParamsFromPage } from'c/navigationUtils';
+
 //custom labels
 import CSP_Print from '@salesforce/label/c.CSP_Print';
 import CSP_Printed_Date from '@salesforce/label/c.CSP_Printed_Date';
 
 export default class PortalCaseDetailsPage extends LightningElement {
     @track now = '';
+
+	@track recordId;
 
     // Expose the labels to use in the template.
     @track label = {
@@ -15,6 +20,15 @@ export default class PortalCaseDetailsPage extends LightningElement {
     //links for images
     logoIcon = CSP_PortalPath + 'CSPortal/Images/Logo/group.svg';
     printIcon = CSP_PortalPath + 'CSPortal/Images/Icons/print-blue.svg';
+
+	connectedCallback() {
+		//get the parameters for this page
+        this.pageParams = getParamsFromPage();
+
+        if(this.pageParams.caseId !== undefined){
+			this.recordId = this.pageParams.caseId;
+        }
+    }
 
     downloadedOn() {
         let d = new Date();
