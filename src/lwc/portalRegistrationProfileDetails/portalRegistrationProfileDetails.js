ef87eb72f601cc03373e6db1e72c93dc3d3cb0a3
Marc Mercier <marc.mercier@isobar.com>
2020-03-16 17:36:26 +0100

    OM-61 : Account Selection is the first step in L2 Registration (ENHC_US_5) - Review banner behavior - Update custom label

--- src/lwc/portalRegistrationProfileDetails/portalRegistrationProfileDetails.js
+++ src/lwc/portalRegistrationProfileDetails/portalRegistrationProfileDetails.js
@@ -60,6 +60,8 @@ export default class PortalRegistrationProfileDetails extends LightningElement {
 
 
     connectedCallback() {
+        this.startLoading();
+
         if (!String.prototype.padStart) {
             String.prototype.padStart = function padStart(targetLength,padString) {
                 targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
@@ -110,9 +112,11 @@ export default class PortalRegistrationProfileDetails extends LightningElement {
                 }
 
                 this.checkCompletion();
+                this.stopLoading();
             })
             .catch((error) => {
                 console.log('Error: ', JSON.parse(JSON.stringify(error)));
+                this.stopLoading();
             });
         this.dispatchEvent(new CustomEvent('scrolltotop'));
     }
