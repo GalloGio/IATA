61b0c7cbd3021f0d3191ec891c6aef3508ade6d7
PedroFartaria <pedro.fartaria@b-i.com>
2020-06-15 13:03:28 +0100

    MTSL-1208 Created component to display breadcrumb for content pages, Added customMetadata to new contentPage reference breadcrumb, adapted current breadcrumb component to retrieve breadcrumb info on load

---
+++ src/lwc/portalContentBreadCrumb/portalContentBreadCrumb.js
@@ -0,0 +1,24 @@
+import { LightningElement,track } from 'lwc';
+
+import getRecomendationDetails from '@salesforce/apex/PortalRecommendationCtrl.getRecomendationDetails';
+ 
+export default class PortalContentHeaderImg extends LightningElement {
+    
+    @track page='FeaturedService';
+    @track showWhite=true;
+    @track serviceTitle;
+
+    connectedCallback(){
+        let contentName =location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+
+        getRecomendationDetails({urlName:contentName})
+        .then(result=>{
+            this.serviceTitle=result.RecommendationTitle__c;
+        })
+        .catch(Error=>{
+           
+        });
+
+    }
+
+}
