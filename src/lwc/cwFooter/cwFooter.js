3acae71b8f972c2e805142675f115cc32743ca28
Igor Moreira <Igor.Moreira@adesso.es>
2020-09-15 16:12:51 +0200

    delployment issue fixed - cwFooter

--- src/lwc/cwFooter/cwFooter.js
+++ src/lwc/cwFooter/cwFooter.js
@@ -1,13 +1,13 @@
 import { LightningElement, api, wire, track } from "lwc";
-import { getSObjectValue } from "@salesforce/apex";
 import pubsub from "c/cwPubSub";
 import getMetadataInfo from "@salesforce/apex/CW_Utilities.getMetadataInfo";
 import getURL from "@salesforce/apex/CW_Utilities.getURLPage";
 import resources from "@salesforce/resourceUrl/ICG_Resources";
 
-import NAME_FIELD from "@salesforce/schema/ICG_Portal_URL__mdt.MasterLabel";
-import URL_FIELD from "@salesforce/schema/ICG_Portal_URL__mdt.Link__c";
-import DESCRIPTION_FIELD from "@salesforce/schema/ICG_Portal_URL__mdt.Description__c";
+const NAME_FIELD = "MasterLabel";
+const URL_FIELD = "Link__c";
+const DESCRIPTION_FIELD = "Description__c";
+
 import { concatinateFacilityAddress, concatinateAddressString, removeLastCommaAddress, removeFromComparisonCommon, saveComparisonListToLocalStorage } from "c/cwUtilities";
 import labels from 'c/cwOneSourceLabels';
 
@@ -55,10 +55,10 @@ export default class cwFooter extends LightningElement {
 	@wire(getMetadataInfo, { mtdName: "Airlines_magazine" }) metadataAirlines;
 
 	get AirlinesName() {
-		return this.metadataAirlines.data ? getSObjectValue(this.metadataAirlines.data, NAME_FIELD) : "";
+		return this.metadataAirlines.data ? this.metadataAirlines.data[NAME_FIELD] : "";
 	}
 	get AirlinesURL() {
-		return this.metadataAirlines.data ? getSObjectValue(this.metadataAirlines.data, URL_FIELD) : "";
+		return this.metadataAirlines.data ? this.metadataAirlines.data[URL_FIELD] : "";
 	}
 
 	// Aviation and the Enviroment
@@ -66,87 +66,87 @@ export default class cwFooter extends LightningElement {
 	metadataAviation;
 
 	get AviationName() {
-		return this.metadataAviation.data ? getSObjectValue(this.metadataAviation.data, NAME_FIELD) : "";
+		return this.metadataAviation.data ? this.metadataAviation.data[NAME_FIELD] : "";
 	}
 	get AviationURL() {
-		return this.metadataAviation.data ? getSObjectValue(this.metadataAviation.data, URL_FIELD) : "";
+		return this.metadataAviation.data ?  this.metadataAviation.data[URL_FIELD] : "";
 	}
 
 	// Twitter
 	@wire(getMetadataInfo, { mtdName: "Twitter" }) metadataTwitter;
 
 	get twitterName() {
-		return this.metadataTwitter.data ? getSObjectValue(this.metadataTwitter.data, NAME_FIELD) : "";
+		return this.metadataTwitter.data ? this.metadataTwitter.data[NAME_FIELD] : "";
 	}
 	get twitterURL() {
-		return this.metadataTwitter.data ? getSObjectValue(this.metadataTwitter.data, URL_FIELD) : "";
+		return this.metadataTwitter.data ? this.metadataTwitter.data[URL_FIELD] : "";
 	}
 
 	// Facebook
 	@wire(getMetadataInfo, { mtdName: "Facebook" }) metadataFacebook;
 
 	get FacebookName() {
-		return this.metadataFacebook.data ? getSObjectValue(this.metadataFacebook.data, NAME_FIELD) : "";
+		return this.metadataFacebook.data ? this.metadataFacebook.data[NAME_FIELD] : "";
 	}
 	get FacebookURL() {
-		return this.metadataFacebook.data ? getSObjectValue(this.metadataFacebook.data, URL_FIELD) : "";
+		return this.metadataFacebook.data ? this.metadataFacebook.data[URL_FIELD] : "";
 	}
 
 	// Linkedin
 	@wire(getMetadataInfo, { mtdName: "LinkedIn" }) metadataLinkedIn;
 
 	get LinkedinName() {
-		return this.metadataLinkedIn.data ? getSObjectValue(this.metadataLinkedIn.data, NAME_FIELD) : "";
+		return this.metadataLinkedIn.data ? this.metadataLinkedIn.data[NAME_FIELD] : "";
 	}
 	get LinkedinURL() {
-		return this.metadataLinkedIn.data ? getSObjectValue(this.metadataLinkedIn.data, URL_FIELD) : "";
+		return this.metadataLinkedIn.data ? this.metadataLinkedIn.data[URL_FIELD] : "";
 	}
 
 	// Youtube
 	@wire(getMetadataInfo, { mtdName: "Youtube" }) metadataYoutube;
 
 	get YoutubeName() {
-		return this.metadataYoutube.data ? getSObjectValue(this.metadataYoutube.data, NAME_FIELD) : "";
+		return this.metadataYoutube.data ? this.metadataYoutube.data[NAME_FIELD] : "";
 	}
 	get YoutubeURL() {
-		return this.metadataYoutube.data ? getSObjectValue(this.metadataYoutube.data, URL_FIELD) : "";
+		return this.metadataYoutube.data ? this.metadataYoutube.data[URL_FIELD] : "";
 	}
 
 	// RSS
 	@wire(getMetadataInfo, { mtdName: "RSS" }) metadataRSS;
 
 	get RSSName() {
-		return this.metadataRSS.data ? getSObjectValue(this.metadataRSS.data, NAME_FIELD) : "";
+		return this.metadataRSS.data ? this.metadataRSS.data[NAME_FIELD] : "";
 	}
 	get RSSURL() {
-		return this.metadataRSS.data ? getSObjectValue(this.metadataRSS.data, URL_FIELD) : "";
+		return this.metadataRSS.data ? this.metadataRSS.data[URL_FIELD] : "";
 	}
 
 	// Legal
 	@wire(getMetadataInfo, { mtdName: "Legal" }) metadataLegal;
 
 	get LegalName() {
-		return this.metadataLegal.data ? getSObjectValue(this.metadataLegal.data, NAME_FIELD) : "";
+		return this.metadataLegal.data ? this.metadataLegal.data[NAME_FIELD] : "";
 	}
 	get LegalURL() {
-		return this.metadataLegal.data ? getSObjectValue(this.metadataLegal.data, URL_FIELD) : "";
+		return this.metadataLegal.data ? this.metadataLegal.data[URL_FIELD] : "";
 	}
 
 	// Legal
 	@wire(getMetadataInfo, { mtdName: "Privacy" }) metadataPrivacy;
 
 	get PrivacyName() {
-		return this.metadataPrivacy.data ? getSObjectValue(this.metadataPrivacy.data, NAME_FIELD) : "";
+		return this.metadataPrivacy.data ? this.metadataPrivacy.data[NAME_FIELD] : "";
 	}
 	get PrivacyURL() {
-		return this.metadataPrivacy.data ? getSObjectValue(this.metadataPrivacy.data, URL_FIELD) : "";
+		return this.metadataPrivacy.data ? this.metadataPrivacy.data[URL_FIELD] : "";
 	}
 
 	// Footer Title
 	@wire(getMetadataInfo, { mtdName: "Footer_Title" }) metadataFooterTitle;
 
 	get FooterTitleDescription() {
-		return this.metadataFooterTitle.data ? getSObjectValue(this.metadataFooterTitle.data, DESCRIPTION_FIELD) : "";
+		return this.metadataFooterTitle.data ? this.metadataFooterTitle.data[DESCRIPTION_FIELD] : "";
 	}
 
 	// Footer Copyright
@@ -154,7 +154,7 @@ export default class cwFooter extends LightningElement {
 	metadataFooterCopyright;
 
 	get FooterCopyrightDescription() {
-		return this.metadataFooterCopyright.data ? getSObjectValue(this.metadataFooterCopyright.data, DESCRIPTION_FIELD) + ' ' + new Date().getFullYear() + '. ' +this.label.all_rights_reserved + '.' : "";
+		return this.metadataFooterCopyright.data ? this.metadataFooterCopyright.data[DESCRIPTION_FIELD] + ' ' + new Date().getFullYear() + '. ' +this.label.all_rights_reserved + '.' : "";
 	}
 
 	logoOther = resources + "/icons/iata-logo-other.svg";
