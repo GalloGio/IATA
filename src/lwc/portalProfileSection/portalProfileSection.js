efb7c35c2aa9658fbf08bfcdea8d7885deca0108
Filipe Carvalho <filipe.carvalho@isobar.com>
2021-04-05 12:20:06 +0100

    IHUB - Header,Detail and Search changes

--- src/lwc/portalProfileSection/portalProfileSection.js
+++ src/lwc/portalProfileSection/portalProfileSection.js
@@ -2,9 +2,16 @@
  * Created by pvavruska on 5/28/2019.
  */
 
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class PortalProfileSection extends LightningElement {
     @api className;
     @api headerClass = 'slds-m-vertical_small';
+    @track titleClass = 'text-medium text-bold text-darkGray';
+
+    connectedCallback() {
+        if(window.location.pathname.includes('service-startuphotlist')){
+            this.titleClass = 'text-medium text-bold ihubBlue' ;   
+        }
+    }
 }
