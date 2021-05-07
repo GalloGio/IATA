d0418d4bfed24d36b9aaee7601dec8edf22acc64
Jorge Adesso <jorge.corral@adesso.es>
2021-05-07 11:10:52 +0200

    Component pending on IATA development

--- src/lwc/serviceInvitation/serviceInvitation.js
+++ src/lwc/serviceInvitation/serviceInvitation.js
@@ -6,10 +6,13 @@ import status from '@salesforce/label/c.Status';
 import role from '@salesforce/label/c.Role';
 import cancel from '@salesforce/label/c.Button_Cancel';
 import invite from '@salesforce/label/c.Invite';
+import pageNumbering from '@salesforce/label/c.Page_Numbering';
 
 //import user id
 import userId from '@salesforce/user/Id';
-import isServiceAdministrator from '@salesforce/apex/InvitationService.isServiceAdministrator';
+// import isServiceAdministrator from '@salesforce/apex/InvitationService.isServiceAdministrator';
+// import getRoles from '@salesforce/apex/InvitationService.getRoles';
+// import getInvitationList from '@salesforce/apex/InvitationService.getInvitationList';
 
 
 export default class ServiceInvitation extends LightningElement {
@@ -19,23 +22,56 @@ export default class ServiceInvitation extends LightningElement {
         status,
         role,
         cancel,
-        invite
+        invite,
+        pageNumbering
 	};
 
     paramKey = 'serviceId';
     serviceId = this.getUrlParamValue(window.location.href, this.paramKey);
 
+    pageNo = 1;
+    recordsPerPage = 10;
+    
+    // @wire(getInvitationList, { serviceId : '$serviceId' })
+    invitationEntireList = [
+        {id:'invite1', email:'test1@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite2', email:'test2@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite3', email:'test3@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite4', email:'test4@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite5', email:'test5@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite6', email:'test6@email.com', role: 'Admin', status: 'Active', isActive: true},
+        {id:'invite7', email:'test7@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite8', email:'test8@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite9', email:'test9@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite10', email:'test10@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite11', email:'test11@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite12', email:'test12@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite13', email:'test13@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite14', email:'test14@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite15', email:'test15@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite16', email:'test16@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite17', email:'test17@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite18', email:'test18@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite19', email:'test19@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite20', email:'test20@email.com', role: 'Admin', status: 'Active', isActive: false},
+        {id:'invite21', email:'test21@email.com', role: 'Admin', status: 'Active', isActive: false}
+    ]
+
     @track invitationInfo = {};
     
-    @wire(isServiceAdministrator, { userId : '$userId' })
-    listUserServices;
+    // @wire(getRoles, { functionalRole : '$' })
+    // roleOptionList;
 
-    connectedCallback(){
-        console.log('Param service id: ' + JSON.stringify(this.serviceId));
-    }
+    // @wire(isServiceAdministrator, { userId : '$userId' })
+    // listUserServices;
 
     get roleOptions(){
-        return [
+        // var options = [];
+        // roleOptionList.forEach(element => {
+        //     options.push({label:element, value:element});
+        // });
+        // return options;
+        return  [
             {label:'Test 1', value:'Test 1'},
             {label:'Test 2', value:'Test 2'},
             {label:'Test 3', value:'Test 3'},
@@ -46,26 +82,36 @@ export default class ServiceInvitation extends LightningElement {
     }
 
     get areInvitationListed(){
-        return true; //this.invitationListed.length !== 0;
+        return this.invitationsToDisplay.length !== 0;
     }
 
     get isServiceAdmin(){
-        return true; //listUserServices.filter(service => service.Id === serviceId)[0].isAdmin;
+        return true; //this.listUserServices.filter(service => service.Id === serviceId)[0].isAdmin;
     }
 
-    get invitationListed(){
-        return [
-            {id:'invite1', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite2', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite3', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite4', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite5', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite6', email:'test@email.com', role: 'Admin', status: 'Active', isActive: true},
-            {id:'invite7', email:'test@email.com', role: 'Admin', status: 'Active', isActive: false},
-            {id:'invite8', email:'test@email.com', role: 'Admin', status: 'Active', isActive: false}
-        ];
+    get totalPages(){
+        return Math.ceil(this.invitationEntireList.length/this.recordsPerPage);
     }
 
+    get invitationsToDisplay(){
+        var begin = (this.pageNo - 1) * parseInt(this.recordsPerPage);
+        var end = parseInt(begin) + parseInt(this.recordsPerPage);
+        return this.invitationEntireList.slice(begin, end);
+    }
+
+    get pageNumberLbl(){
+        return this.label.pageNumbering.replace('{0}', this.pageNo).replace('{1}', this.totalPages);
+    }
+
+    get isFirstPage(){
+        return this.pageNo === 1;
+    }
+
+    get hasMorePages(){
+        return this.pageNo < this.totalPages;
+    }
+
+    // Action functions
     updateField(event){
         const fieldName = event.target.dataset.field;
         const value = event.target.value;
@@ -85,8 +131,25 @@ export default class ServiceInvitation extends LightningElement {
         console.log('Canceling invitation ' + event.target.dataset.id);
     }
 
+    handleNext(){
+       this.pageNo += 1;
+    }
 
-    //Private methods
+    handlePrevious(){
+        this.pageNo -= 1;
+    }
+
+    handleFirst(){
+        this.pageNo = 1;
+    }
+
+    handleLast(){
+        this.pageNo = this.totalPages;
+    }
+
+
+
+    //Private functions
     validateEmail(email){
         return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email);
     }
